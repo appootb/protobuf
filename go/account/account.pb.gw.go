fa8cb4004806444116a1f5c0e5984b30c70ed629
Yuriy Lu <youlu@outlook.com>
2020-08-23 00:21:30 +0800

    Fix: protoc-gen-grpc-gateway generated issue

--- go/account/account.pb.gw.go
+++ go/account/account.pb.gw.go
@@ -33,35 +33,12 @@ var _ = utilities.NewDoubleArray
 var _ = descriptor.ForMessage
 
 var (
-	filter_Account_GetInfo_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_Account_GetInfo_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
 func request_Account_GetInfo_0(ctx context.Context, marshaler runtime.Marshaler, client AccountClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq common.UniqueId
 	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
-	}
-
-	if protoReq.Value == nil {
-		protoReq.Value = &UniqueId_Id{}
-	} else if _, ok := protoReq.Value.(*UniqueId_Id); !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "expect type: *UniqueId_Id, but: %t\n", protoReq.Value)
-	}
-	protoReq.Value.(*UniqueId_Id).Id, err = runtime.Uint64P(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
-	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -78,29 +55,6 @@ func request_Account_GetInfo_0(ctx context.Context, marshaler runtime.Marshaler,
 func local_request_Account_GetInfo_0(ctx context.Context, marshaler runtime.Marshaler, server AccountServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq common.UniqueId
 	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
-	}
-
-	if protoReq.Value == nil {
-		protoReq.Value = &UniqueId_Id{}
-	} else if _, ok := protoReq.Value.(*UniqueId_Id); !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "expect type: *UniqueId_Id, but: %t\n", protoReq.Value)
-	}
-	protoReq.Value.(*UniqueId_Id).Id, err = runtime.Uint64P(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
-	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -390,7 +344,7 @@ func RegisterAccountHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 }
 
 var (
-	pattern_Account_GetInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"account", "info", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Account_GetInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"account", "info"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Account_GetInfos_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"account", "infos", "ids"}, "", runtime.AssumeColonVerbOpt(true)))
 
